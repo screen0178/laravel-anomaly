e314331587a80995183b663064d66af1cb18a1d9
screen24 <zhafari2405@gmail.com>
2020-12-07 13:52:57 +0700

    fixed timestamp

--- Modules/Anomaly/Entities/KaiRaw.php
+++ Modules/Anomaly/Entities/KaiRaw.php
@@ -49,28 +49,6 @@ class KaiRaw extends Model
     protected $casts = [
         'data' => 'array'
     ];
-
-    /**
-     * The attributes that should be mutated to dates.
-     *
-     * @var array
-     */
-    protected $dates = [
-        
-    ];
-
-    /**
-     * Indicates if the model should be timestamped.
-     *
-     * @var boolean
-     */
-    public $timestamps = false;
-
-    // Scopes...
-
-    // Functions ...
-
-    // Relations ...
     
     protected static function newFactory()
     {
